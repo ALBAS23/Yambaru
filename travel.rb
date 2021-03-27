0fdded0378939af481cdeb4f91baf4367b5c9c23
ALBAS23 <maxbouxmasa3939@gmail.com>
2021-03-28 08:32:57 +0900

    修正１ 条件分岐：旅行プログラム

--- travel.rb
+++ travel.rb
@@ -5,43 +5,42 @@ puts <<~TEXT
 3. 九州旅行（15000円）
 TEXT
 
-i = 0
-while(i == 0)
+while true
   print "プランの番号を選択 > "
-  plan = gets.to_i
-  if plan == 1
-    puts "沖縄旅行ですね。"
-    price = 10000
-    i = 1
-  elsif plan == 2
-    puts "北海道旅行ですね"
-    price = 20000
-    i = 1
-  elsif plan == 3
-    puts "九州旅行ですね"
-    price = 15000
-    i = 1
-  else
-    puts "1~3の番号を入力してください。"
+  case plan_select = gets.to_i
+  when 1, 2, 3
+    break
   end
+  puts "1〜3の番号を入力して下さい。"
 end
 
-i = 0
-while(i == 0)
+case plan_select
+when 1
+  plan_name = "沖縄旅行"
+  plan_price = 10000
+when 2
+  plan_name = "北海道旅行"
+  plan_price = 20000
+when 3
+  plan_name = "九州旅行"
+  plan_price = 15000
+end
+puts "#{plan_name}ですね。"
+
+while true
   puts "何名で予約されますか？"
   print "人数を入力 > "
-  num = gets.to_i
-  sum = price * num
-  if num >= 5
-    puts "#{num}名ですね。"
-    puts "5名以上ですので10%割引となります"
-    puts "合計料金は#{(sum * 0.9).to_i}円になります。"
-    i = 1
-  elsif 1 <= num && num <= 4
-    puts "#{num}名ですね。" 
-    puts "合計料金は#{sum}円になります。"
-    i = 1
-  else
-    puts "1以上を入力して下さい。"
+  people_num = gets.to_i
+  if 1 <= people_num
+    break
   end
+  puts "1以上を入力して下さい。"
 end
+
+total_sum = plan_price * people_num
+puts "#{people_num}名ですね。"
+if 5 <= people_num
+  puts "5名以上ですので10%割引となります"
+  total_sum = (total_sum * 0.9).to_i
+end
+puts "合計料金は#{total_sum}円になります。"
